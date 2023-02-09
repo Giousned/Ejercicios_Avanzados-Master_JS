d4da12bbec98cd01318dfde8e0bf285dce241774
Giousned <guille_ranza@hotmail.com>
2023-02-09 16:52:49 +0000

    [ADD] 2 ejercicios mas

--- exercises/154-searchWithDivideAndConquer/app.js
+++ exercises/154-searchWithDivideAndConquer/app.js
@@ -1,49 +1,29 @@
 function search(array, value) {
   // your code here
 
-  let suma = 0;
-  let media = 0;
+    let start = 0
+    let end = array.length - 1;
 
-  let corte = 0;
+    while (start <= end) {
 
-  let newArr = [...array];
+         let mid = Math.floor((start + end) / 2);
 
-  do {
+         if (array[mid] === value) return mid;
 
-    suma = newArr.reduce((a,b) => a+b, 0);
-    console.log(suma);
-    media = Math.floor(suma/newArr.length);
-    console.log(media);
-    corte = Math.floor(newArr.length/2);
+         else if (array[mid] < value)
+              start = mid + 1;
 
-    if (media == value) break;
-
-    else if (array.length%2 === 0){
-      if (media < value) {newArr = newArr.slice(corte);
-        console.log(newArr);}
-    
-      else {newArr = newArr.slice(0,corte);}
+         else
+              end = mid - 1;
     }
-
-    else {
-      if (media < value) {newArr = newArr.slice(corte+1);
-        console.log(newArr);}
-    
-      else {newArr = newArr.slice(0,corte); console.log(newArr);}      
-    }
-
-  } while (newArr.length != 0);
-
-  if (array.indexOf(media) != - 1 && newArr.length != 0) return Number(array.indexOf(media));
-  else return null;
-
+    return null;
 }
 
 let arr = [1, 3, 16, 22, 31, 33, 34]
 console.log(search(arr, 31)); // => 4
 
-// let arr2 = [1, 2]
-// console.log(search(arr2, 2)); // => 1 numero
+let arr2 = [1, 2]
+console.log(search(arr2, 2)); // => 1 numero
 
-// let arr3 = [1, 3, 16, 22, 31, 33, 34]
-// console.log(search(arr3, 3)); // => 1
+let arr3 = [1, 3, 16, 22, 31, 33, 34]
+console.log(search(arr3, 3)); // => 1
--- exercises/156-isogram/app.js
+++ exercises/156-isogram/app.js
@@ -3,8 +3,52 @@ function isIsogram(text) {
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
   // note: a set drops dup values
   // thus, to see if all the chars were unique,
-  // check length of text and the size of the set 
+  // check length of text and the size of the set
+
+  if (text.length === 0) return true;
+
+  let objSet = {};
+  let cadena = text.toLowerCase();
+
+  objSet = new Set(cadena);
+
+  if(cadena.length !== objSet.size) return false
+    else return true
 }
 
 console.log(isIsogram("Camile")) // => true
 console.log(isIsogram("Camille")) // => false
+
+/* OTRA SOLUCION
+function isIsogram(text) {
+  if (text.length === 0) return true;
+
+  let obj = {};
+  let cadena = text.toLowerCase();
+  let valorRepe = [];
+
+  for (let i of cadena){
+    if(obj[i]) obj[i] += 1;
+    else obj[i] = 1; 
+  }
+
+  valorRepe = Object.values(obj).filter(e => e>1);
+
+  if(valorRepe.length>0) return false
+    else return true
+}
+*/
+
+/* SOLUCION DADA
+function isIsogram(text) {
+  const chars = {};
+  for (const char of text) {
+    chars[char] = (chars[char] || 0) + 1;
+  }
+  let result = Object.entries(chars)
+    .filter((char) => char[1] > 1)
+    .map((char) => char[0]);
+  if (result.length > 0) return false;
+  else return true;
+}
+*/
